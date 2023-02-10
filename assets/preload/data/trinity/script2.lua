8414f0ab3403b28f99d63804f7221ea03f365ec3
TheGuyWhoPorts <121072594+TheGuyWhoPorts@users.noreply.github.com>
2023-02-10 07:32:07 -0500

    Add files via upload

---
+++ assets/preload/data/trinity/script2.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    setProperty('scoreTxt.visible', true)
+	    setProperty('showRating', false)
+    	    setProperty('showComboNum', false)
+	    setProperty('showCombo', false)
+end
